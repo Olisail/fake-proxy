045471a32c7217c525a88253b5e834905c462c52
Olivier SAILLY <olivier.sailly.59@gmail.com>
2022-08-17 09:19:28 +0200

    feat: enable TLS
    Also added a readme.md for obvious reasons

--- proxy.js
+++ proxy.js
@@ -1,9 +1,28 @@
+const path = require('path');
+const { execSync } = require('child_process');
+const { readFileSync } = require('fs');
 const hoxy = require('hoxy');
 
+const rootCAFolder = execSync('mkcert -CAROOT').toString('utf-8').trim();
+
+const certLocation = path.join(rootCAFolder, 'rootCA.pem');
+const keyLocation = path.join(rootCAFolder, 'rootCA-key.pem');
+console.info(`Expecting root CA Cert in '${certLocation}' and Key in '${keyLocation}'.`);
+
+const certAuthority = {
+    cert: readFileSync(certLocation),
+    key: readFileSync(keyLocation),
+};
+console.info('Root CA loaded successfully!');
+
 const port = 5000;
-const proxy = hoxy.createServer().listen(port, () => console.log(`The proxy is listening on port ${port}.`));
+const proxy = hoxy.createServer({ certAuthority }).listen(port, () => console.info(`The proxy is listening on port ${port}.`));
 
 proxy.log('error warn', process.stderr);
 proxy.log('info', process.stdout);
 
-proxy.intercept({ phase: 'request' }, req => console.log(`Forwarding request: ${req.fullUrl()}`));
+proxy.intercept({ phase: 'request' }, req => console.info(`Forwarding request: ${req.fullUrl()}`));
+
+// We do not want to throw here because the proxy should continue to run even though it met an error
+proxy.on('error', err => console.error('Error detected!', err));
+process.on('uncaughtException', err => console.error('Uncaught Exception detected!', err));
---
+++ readme.md
@@ -0,0 +1,19 @@
+# How to
+
+_For now, this project is targetting Windows (personal needs)._
+
+## Notes
+
+This should work with node LTS (16.17.0 as of today).
+
+This project was created in order for offshore colleagues to have a pass-through proxy running under the same port as local developers. With this, we can run the same project and set of settings on different computer configurations.
+
+## Certificates
+
+1. Install [mkcert](https://github.com/FiloSottile/mkcert).
+2. Run `mkcert -install`
+
+## Install and Run
+
+1. Run `npm i`
+2. Run `npm run start`
